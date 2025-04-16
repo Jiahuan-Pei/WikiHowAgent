96372e84fc51deb7c51589c01fd6775a2af9db5d
Jiahuan Pei <33063105+Jiahuan-Pei@users.noreply.github.com>
2025-04-16 16:34:42 +0200

    trival

--- Agents/learner_agent.py
+++ Agents/learner_agent.py
@@ -38,7 +38,7 @@ class LearnerLLMAgent:
             learner_response = re.sub(r'[^\x00-\x7F]+', '', learner_response)
 
         except:
-            print('Learner:INVOKE:ERR', data, 'Please check if you have the current llm or not.')
+            print('Learner:INVOKE:ERR', data, 'Please check if you have the current llm or your API key is valid.')
 
         print('<<🤔 Learner>>', learner_response)
         return learner_response
